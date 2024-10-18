de461a063476280d7f4d846c1126577facfc5d33
Jesús Canales <177686251+jesus-canales@users.noreply.github.com>
2024-10-18 03:34:40 +0000

    Ejercicio sobre resaltar elementos con javascript

---
+++ 04_JS/05_Resaltando_Personajes/js/app.js
@@ -0,0 +1,20 @@
+// 01 Asignando un evento click sobre el subtítulo
+const miSubtitulo = document.getElementById('subtitulo');
+miSubtitulo.addEventListener('click', function(){
+    console.log('Haz hecho click');
+});
+
+// 02 Ahora vamos a establecer que cuando hago click sobre un elemento cambie a color rojo y fondo amarillo
+const heroesAvengers = document.querySelectorAll('.listAvenger');
+for(let i = 0; i < heroesAvengers.length; i++){
+    heroesAvengers[i].addEventListener('click', colorRojo);
+};
+
+// Utilizamos el estilo red
+function colorRojo(){
+    console.log(this);
+    let avengerSeleccionado = this.classList.toggle("red");
+};
+
+// RETO: establecer que cuando se haga click aparezca la imagen respectiva del Avenger seleccionado
+
